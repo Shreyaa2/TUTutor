8cfc1618e2905e9c4b0cf0c8d365b522d3cec98b
Yi-Shan <43280619+yishanstone@users.noreply.github.com>
2018-10-19 22:04:02 -0400

    Delete tututor.js

--- tututor.js
+++
@@ -1,8 +0,0 @@
-function checkPassword(f){
-	var pw = document.forms["tutorform"]["tutorpassword"].value
-	var vpw = document.forms["tutorform"]["verifytutorpassword"].value
-	if (pw !== vpw){
-		alert("Make sure you enter the same password!");
-		return false;
-	}
-}
