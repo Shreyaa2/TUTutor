c691733ecae374b9f106ccfcfb0f8f968684af3a
jcaylo1 <jcaylo1@students.towson.edu>
2018-10-21 10:07:39 -0400

    Update tututor.js

--- tututor.js
+++ tututor.js
@@ -1,16 +1,16 @@
-function checkPasswordTutor(f){
+function checkPasswordTutor(){
 	var pw_t = document.forms["tutorform"]["tutorpassword"].value
 	var vpw_t = document.forms["tutorform"]["verifytutorpassword"].value
-	if (pw !== vpw){
+	if (pw_t !== vpw_t){
 		alert("Make sure you enter the same password!");
 		return false;
 	}
 }
 
-function checkPasswordPupil(f){
+function checkPasswordPupil(){
 	var pw_p = document.forms["pupilform"]["pupilpassword"].value
 	var vpw_p = document.forms["pupilform"]["verifypupilpassword"].value
-	if (pw !== vpw){
+	if (pw_p !== vpw_p){
 		alert("Make sure you enter the same password!");
 		return false;
 	}
