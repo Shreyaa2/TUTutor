cdc139f733c768d3b76bc83ac0daea7fcedc63a0
jcaylo1 <jcaylo1@students.towson.edu>
2018-10-21 09:04:31 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,29 @@
-//  var express = require('express');
-// var app =express();
+ var express = require('express');
+ var app =express();
 
-//  //set port
-// //Heroku is going to set the port or if Heroku can't, then we will be at port 8080
-// var port = process.env.PORT || 8080
+  //set port
+ //Heroku is going to set the port or if Heroku can't, then we will be at port 3000
+ var port = process.env.PORT || 3000
 
-//  //allows use to have static files
-//  app.use(express.static(__dirname));
+  //allows use to have static files
+ app.use(express.static(__dirname));
 
-//  //routes
+  //routes
 
 
-//  app.get("/", function(req,res){
-//      res.render("tututor_mainpage");
-//  })
+ app.get("/", function(req,res){
+      res.render("tututor_mainpage");
+ })
 
-//  app.listen(port, function(){
-//   console.log("app running");
-//  })
+  app.listen(port, function(){
+  console.log("app running");
+  })
 
-var http = require('http'),
-    fs = require('fs')
-var port = process.env.PORT || 8080
-http.createServer(function(req, res) {
-    var url = './' + (req.url == '/' ? 'tututor_mainpage.html' : req.url)
-    fs.readFile(url, function(err, html) {
-        if (err) {
-            var message404 = "There is no such page! <a href='/'>Back to home page</a>"
-            res.writeHead(404, {'Content-Type': 'text/html', 'Content-Length': message404.length})
-            res.write(message404)
-        } else {
-            res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': html.length})
-            res.write(html)
-        }
-        res.end()
-    })
-}).listen(port)
+app.use(function (req, res){
+    res.status(404).send("Sorry, page does not exist!)
+                         })
+ 
+app.use(function (err,req, res){
+    console.error(err.stack)
+    res.status(500).send("Something went wrong!)
+                         })
