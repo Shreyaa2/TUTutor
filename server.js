0fb344e418be651b6ded658dab100b28cef48e67
Shreya <34201376+Shreyaa2@users.noreply.github.com>
2018-10-29 21:22:27 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
  var express = require('express');
  var app =express();
 var path= require('path');
+var bodyparse = required('body-parse');
+//import route for pupildata
+import prouter from './pupilroute';
 
   //set port
  //Heroku is going to set the port or if Heroku can't, then we will be at port 3000
@@ -8,6 +11,15 @@ var path= require('path');
 
   //allows use to have static files
  app.use(express.static(__dirname));
+
+ //allows use to have static files like style sheets and js/script
+ app.use(express.static(__dirname));
+ app.use('/models/pupil', prouter);
+
+ //looks at incoming data and parses it depending on it coming from a JSON or data from a form
+ app.use(bodyparse.JSON());
+ app.use(bodyparse.urlencoded({ extended: true }));
+
 
   //routes
 
