4f82025044cefd194909d1c7f57a45eb81029162
Shreayaa <shreyaa2095@gmail.com>
2018-11-07 16:01:20 -0500

    Update indexroutes.js

--- indexroutes.js
+++ indexroutes.js
@@ -6,7 +6,7 @@ var router = express.Router();
 //importing the controller to implement http requests
 var pupil_controller = require('./pupils');
 
-router.post('/book/create', pupil_controller.create_pupilpro);
+
 
 module.exports = router;
 
