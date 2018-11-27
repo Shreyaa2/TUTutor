a9e419ddfdeaf235424793b58ede7c0d3b2e0b22
jcaylo1 <jcaylo1@students.towson.edu>
2018-11-26 20:19:52 -0500

    Update server.js

--- server.js
+++ server.js
@@ -32,20 +32,20 @@ mongoose.Promise = global.Promise;
  var jsonParser = bodyparse.json();
  //app.use(jsonParser.urlencoded({extended: false}));
 
-//  app.use((req, res, next) => {
-//     const error = new Error("Not found");
-//     error.status = 404;
-//     next(error);
-//   });
+ app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+  });
   
-//   app.use((error, req, res, next) => {
-//     res.status(error.status || 500);
-//     res.json({
-//       error: {
-//         message: error.message
-//       }
-//     });
-// });
+  app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+      error: {
+        message: error.message
+      }
+    });
+});
 
 
 //allows use to have static files like style sheets and js/script
