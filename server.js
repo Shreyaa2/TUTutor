eadfff3a34236440e7b4fb6e4514d9d882c40297
jcaylo1 <jcaylo1@students.towson.edu>
2018-11-26 20:31:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,18 +3,18 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var bodyparse = require('body-parser');
-//const morgan = require('morgan');
+const morgan = require('morgan');
 app.use(bodyparse.json());
 app.use(bodyparse.urlencoded({extended: false}));
-//app.use(morgan("dev"));
+app.use(morgan("dev"));
 var mongoose = require('mongoose');
 //importing controller
 //var indexr= require('./indexroutes');
 var pupil = require('./pupils');
 
 
-process.env.MONGODB = 'mongodb://admin:cozysweater18!@ds039707.mlab.com:39707/heroku_l1frxk38';
-mongoose.connect(process.env.MONGODB, {useNewUrlParser: true}, {useMongoClient: true});
+process.env.MONGODB = 'mongodb://admin:cozysweater18!@ds223763.mlab.com:23763/towsont';
+mongoose.connect(process.env.MONGODB, {useNewUrlParser: true});
 
 //mongoose.connection.on('error', console.error.bind(console, 'connection error'));
 mongoose.connection.on('error', function () {
@@ -26,7 +26,7 @@ mongoose.Promise = global.Promise;
 
  //set port
  //Heroku is going to set the port or if Heroku can't, then we will be at port 3000
- var port = process.env.port || 3000;
+ var port = process.env.PORT || 3000;
 
   //looks at incoming data and parses it depending on it coming from a JSON or data from a form
  var jsonParser = bodyparse.json();
@@ -55,15 +55,12 @@ app.use(express.static(__dirname));
       res.sendFile(path.join(__dirname+'/tututor_mainpage.html'));
  });
 
-app.get('./pupilprofile', function (req, res){
-res.status(200);
-console.log('pupil get');
-});
+//  app.post("/", function(req,res){
+//      res.sendFile(path.join(__dirname+'GitHub/TUTutor/pupilpro'))
+//  })
 
 //app.use('pupilcontroller', jsonParser,indexr);
-
-app.post('./pupilprofile', function (req, res){
-
+app.post('/pupilpro', function (req, res){
     var pupili = new pupil (
         {
          pfname: req.body.pupilFirst,
@@ -71,16 +68,18 @@ app.post('./pupilprofile', function (req, res){
          pemail: req.body.pupilemail,
          ppassword: req.body.pupilpassword,
          pvpassword: req.body.verifypupilpassword,
-         pmajor: req.body.pupilMajor
+         pmajor: req.body.pupilMajor,
+         plevel: req.body.academicYear
         },
         pupili.save(function (err, pupil){
             if (err) return console.error(err);
             res.status(201);
-          //  console.log(pupil.pfname + 'saved to collection');
-        }));
+            console.log(pupil.fname + ' saved to collection');
+        })
+    );
     });
 
-// mongoose.connection.close();
+ //mongoose.connection.close();
   app.listen(port, function(){
   console.log(`Listening on port ${port}`);
   });
