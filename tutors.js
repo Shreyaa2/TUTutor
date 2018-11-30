54c8535c613c78d868fa5eaf6d0239c25d89ce99
Shreayaa <shreyaa2095@gmail.com>
2018-11-30 02:45:12 -0500

    Update tutors.js

--- tutors.js
+++ tutors.js
@@ -3,14 +3,14 @@ const mongoose = require('mongoose');
 var tutuorschema = mongoose.Schema ({
     tfname: { type: String, required: true},
     tlname: {type: String, required: true},
-    temil: { type: String},
+    temail: { type: String},
     tpassword: { type: String, required: true}, 
     tvpassword: { type: String, required: true}, 
     tmajor: { type: String},
     tlevel: {type: String},
     tcourse: {type: String},
-    trate: {type: String},
-    tdesc: {type: String},
+    trate: {type: Number},
+    tdesc: {type: String },
 });
 
 var tutor = mongoose.model('tutor', tutuorschema, 'tutorinfo');
