77ec60941121c9f4a42667cac87072855b195635
Shreayaa <shreyaa2095@gmail.com>
2018-11-30 02:30:28 -0500

    Update tutorroute.js

---
+++ tutorroute.js
@@ -0,0 +1,53 @@
+var express = require('express');
+var router = express.Router();
+var app = express();
+var bodyparse = require('body-parser');
+var j = bodyparse.json();
+var urlencodedParser = bodyparse.urlencoded({extended: true});
+app.use(bodyparse.json());
+var $ = require('jquery');
+
+const tutor = require('./tutors');
+
+router.post('/', urlencodedParser,j,function (req, res){
+    var pupili = new pupil (
+        {
+         pfname: req.body.pupilFirst,
+         plname: req.body.pupilLast,
+         pemail: req.body.pupilEmail,
+         ppassword: req.body.pupilpassword,
+         pvpassword: req.body.verifypupilpassword,
+         pmajor: req.body.pupilMajor,
+         plevel: req.body.academicYear,
+        });
+        pupili.save(function (err, pupil){
+            if (err) return console.error(err);
+             res.status(201);
+          res.redirect('http://www.google.com'); 
+        });    
+    });
+
+router.get('/', urlencodedParser,j,function(req, res){
+    res.status(200);
+});
+
+router.post('/', urlencodedParser,j,function (req, res) {
+    var tutori = new tutor ({
+        tfname: req.body.tutorFirst,
+        tlname: req.body.tutorLast,
+        temil: req.body.tutorEmail,
+        tpassword: req.body.tutorpasword, 
+        tvpassword: req.body.verifytutorpassword,
+        tmajor: req.body.tutorMajor,
+        tlevel: req.body.academicYear,
+        tcourse: req.body.tutorCourse,
+        trate: req.body.tutorRate,
+    });
+    tutori.save(function (err){
+        if (err) return console.error(err);
+        res.status(201);
+        res.redirect('https://towsonu-tutor.herokuapp.com/homepage.html');    
+    });
+});
+
+module.exports = router2;
