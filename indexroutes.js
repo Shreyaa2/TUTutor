3b17a9391fbd4e11ae244dfba926aeadebceba97
Shreayaa <shreyaa2095@gmail.com>
2018-11-30 02:04:22 -0500

    Update indexroutes.js

--- indexroutes.js
+++ indexroutes.js
@@ -11,11 +11,9 @@ var $ = require('jquery');
 
 //mongoose schema
 const pupil = require('./pupils');
+const tutor = require('./tutors');
 //importing the controller to implement http requests
 //var pupil_controller = require('./pupils');
-
-
-
 
 router.get('/',urlencodedParser,j,function (req, res){
     res.status(200).json({
@@ -41,6 +39,28 @@ router.post('/', urlencodedParser,j,function (req, res){
         });    
     });
 
+router.get('/', urlencodedParser,j,function(req, res){
+    res.status(200);
+});
+
+router.post('/', urlencodedParser,j,function (req, res) {
+    var tutori = new tutor ({
+        tfname: req.body.tutorFirst,
+        tlname: req.body.tutorLast,
+        temil: req.body.tutorEmail,
+        tpassword: req.body.tutorpasword, 
+        tvpassword: req.body.verifytutorpassword,
+        tmajor: req.body.tutorMajor,
+        tlevel: req.body.academicYear,
+        tcourse: req.body.tutorCourse,
+        trate: req.body.tutorRate,
+    });
+    tutori.save(function (err){
+        if (err) return console.error(err);
+        res.status(201);
+        res.redirect('https://towsonu-tutor.herokuapp.com/homepage.html');    
+    });
+});
 
 module.exports = router;
 
