04bb1c5a2fbe19975e44822e1bd7103aa1831bb2
Shreayaa <shreyaa2095@gmail.com>
2018-11-30 02:30:30 -0500

    Update indexroutes.js

--- indexroutes.js
+++ indexroutes.js
@@ -11,7 +11,7 @@ var $ = require('jquery');
 
 //mongoose schema
 const pupil = require('./pupils');
-const tutor = require('./tutors');
+
 //importing the controller to implement http requests
 //var pupil_controller = require('./pupils');
 
@@ -39,28 +39,28 @@ router.post('/', urlencodedParser,j,function (req, res){
         });    
     });
 
-router.get('/', urlencodedParser,j,function(req, res){
-    res.status(200);
-});
+// router.get('/', urlencodedParser,j,function(req, res){
+//     res.status(200);
+// });
 
-router.post('/', urlencodedParser,j,function (req, res) {
-    var tutori = new tutor ({
-        tfname: req.body.tutorFirst,
-        tlname: req.body.tutorLast,
-        temil: req.body.tutorEmail,
-        tpassword: req.body.tutorpasword, 
-        tvpassword: req.body.verifytutorpassword,
-        tmajor: req.body.tutorMajor,
-        tlevel: req.body.academicYear,
-        tcourse: req.body.tutorCourse,
-        trate: req.body.tutorRate,
-    });
-    tutori.save(function (err){
-        if (err) return console.error(err);
-        res.status(201);
-        res.redirect('https://towsonu-tutor.herokuapp.com/homepage.html');    
-    });
-});
+// router.post('/', urlencodedParser,j,function (req, res) {
+//     var tutori = new tutor ({
+//         tfname: req.body.tutorFirst,
+//         tlname: req.body.tutorLast,
+//         temil: req.body.tutorEmail,
+//         tpassword: req.body.tutorpasword, 
+//         tvpassword: req.body.verifytutorpassword,
+//         tmajor: req.body.tutorMajor,
+//         tlevel: req.body.academicYear,
+//         tcourse: req.body.tutorCourse,
+//         trate: req.body.tutorRate,
+//     });
+//     tutori.save(function (err){
+//         if (err) return console.error(err);
+//         res.status(201);
+//         res.redirect('https://towsonu-tutor.herokuapp.com/homepage.html');    
+//     });
+// });
 
 module.exports = router;
 
