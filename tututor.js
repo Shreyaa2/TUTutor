79b74658b10fbc30c4201d894924c67d312e035e
Shreya <34201376+Shreyaa2@users.noreply.github.com>
2018-11-29 19:36:44 -0500

    Update tututor.js

--- tututor.js
+++ tututor.js
@@ -86,7 +86,7 @@ function checkLogin(){
 
 function submit(){
     $(document).ready(function (){
-        const url = 'https://towsonu-tutor.herokuapp.com/pupilprofile/pupilpro';
+        const url = 'https://towsonu-tutor.herokuapp.com/pupilpro';
         var data = {
           d: 'document.form',
 
