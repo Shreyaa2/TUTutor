5bb8ed9eabbf4d421505598755aaae47b9ca4cf5
Shreayaa <shreyaa2095@gmail.com>
2018-12-01 15:41:34 -0500

    Update Routers/Login.js

--- Routers/Login.js
+++ Routers/Login.js
@@ -26,7 +26,7 @@ router.get('/' ,urlencodedParser, j, (req, res, next) => {
               });
             }
           if (result) {
-          res.sendStatus(200).json({
+          res.status(200).json({
               message: 'Authentication successful'
           });
         }
