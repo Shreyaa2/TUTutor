5936851f49740ad8f4a67032e1399f4605c825a1
Shreayaa <shreyaa2095@gmail.com>
2018-12-01 17:29:27 -0500

    l2

--- server.js
+++ server.js
@@ -55,8 +55,8 @@ app.get("/", function(req, res){
 
  app.use('/pupilpro', pupilroutes);
  app.use('/tutorpro', tutorroutes);
- app.use('/signin/student', login);
- app.use('/signin/tutor', tlogin);
+ app.use('/signs', login);
+ app.use('/signt', tlogin);
 
 //  app.use(function (req, res, next) {
 //     const error = new Error('Not Found');
