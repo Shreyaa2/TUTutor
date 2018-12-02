0b7426c28296f3adf7091d0c777454b4e6919730
Yi-Shan <43280619+yishanstone@users.noreply.github.com>
2018-12-02 12:15:46 -0500

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,25 +1,49 @@
-//const e = React.createElement;
+////const e = React.createElement;
 
-class Greets extends React.Component{
-    //constructor(props) {
-    //    super(props);
-    //    this.state = {  };
-    //}
+//class Greets extends React.Component{
+    ////constructor(props) {
+    ////    super(props);
+    ////    this.state = {  };
+    ////}
     
-    render(){
-        if (document.getElementById('pupilFirst')) {
-           // (document.forms["pupilform"].getElementById('sub').clicked == true)
-            var name = document.forms["pupilform"]["pupilFirst"].value;   
-        } else if (document.getElementById('tutorFirst')){
-            var name = document.forms["tutorform"]["tutorFirst"].value;            
-        } else {
-            var name = 'anonymous';
-        }
-        return <h3>Hello, {name}!</h3>;
+    //render(){
+      //  if (document.getElementById('pupilFirst')) {
+        //   // (document.forms["pupilform"].getElementById('sub').clicked == true)
+        //    var name = document.forms["pupilform"]["pupilFirst"].value;   
+        //} else if (document.getElementById('tutorFirst')){
+        //    var name = document.forms["tutorform"]["tutorFirst"].value;            
+        //} else {
+        //    var name = 'anonymous';
+        //}
+        //return <h3>Hello, {name}!</h3>;
         
+    //}
+//}
+
+//const domContainer = document.getElementById('greeting');
+////.querySelector('#greeting');
+//ReactDOM.render(<Greets />, domContainer);
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return document.forms["pupilform"]["pupilFirst"].value;
     }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
 }
 
-const domContainer = document.getElementById('greeting');
-//.querySelector('#greeting');
-ReactDOM.render(<Greets />, domContainer);
+const domContainer = document.querySelector('#greeting');
+ReactDOM.render(e(LikeButton), domContainer);
