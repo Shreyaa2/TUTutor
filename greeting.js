3f47744eef8f3d7f7aa5825dbfdf397b6bf14b89
Yi-Shan <43280619+yishanstone@users.noreply.github.com>
2018-12-02 00:14:27 -0500

    not done

---
+++ greeting.js
@@ -0,0 +1,25 @@
+const e = React.createElement;
+
+class Greets extends React.Component{
+    //constructor(props) {
+    //    super(props);
+    //    this.state = {  };
+    //}
+    
+    render(){
+        if (document.getElementById('pupilFirst')) {
+           // (document.forms["pupilform"].getElementById('sub').clicked == true)
+            var name = document.forms["pupilform"]["pupilFirst"].value;   
+        } else if (document.getElementById('tutorFirst')){
+            var name = document.forms["tutorform"]["tutorFirst"].value;            
+        } else {
+            var name = 'anonymous';
+        }
+        return <h3>Hello, {name}!</h3>;
+        
+    }
+}
+
+const domContainer = document.getElementById('greeting');
+//.querySelector('#greeting');
+ReactDOM.render(e(Greets), domContainer);
