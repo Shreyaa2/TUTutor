4cb818f0165b64461d9cfee3be1c35fe303c9cb4
Yi-Shan <43280619+yishanstone@users.noreply.github.com>
2018-12-03 03:45:55 -0500

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,31 +1,4 @@
-////const e = React.createElement;
-
-//class Greets extends React.Component{
-    ////constructor(props) {
-    ////    super(props);
-    ////    this.state = {  };
-    ////}
-    
-    //render(){
-      //  if (document.getElementById('pupilFirst')) {
-        //   // (document.forms["pupilform"].getElementById('sub').clicked == true)
-        //    var name = document.forms["pupilform"]["pupilFirst"].value;   
-        //} else if (document.getElementById('tutorFirst')){
-        //    var name = document.forms["tutorform"]["tutorFirst"].value;            
-        //} else {
-        //    var name = 'anonymous';
-        //}
-        //return <h3>Hello, {name}!</h3>;
-        
-    //}
-//}
-
-//const domContainer = document.getElementById('greeting');
-////.querySelector('#greeting');
-//ReactDOM.render(<Greets />, domContainer);
-
 const e = React.createElement;
-//var nameP = document.getElementById("pupilFirst").value;
 
 class LikeButton extends React.Component {
   constructor(props) {
@@ -35,13 +8,7 @@ class LikeButton extends React.Component {
 
   render() {
     if (this.state.clicked) {
-        //var name = document.forms["pupilform"]["pupilFirst"].value;
-        //var nameS = name.toString();
       return 'You have come to the right place!'
-        //this.props.pname;
-        //nameP;
-        //'Hello!';
-        //document.forms["pupilform"]["pupilFirst"].value;
     }
 
     return e(
