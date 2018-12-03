fd8884fadc616f1fdac5e3da468993f4ea928c31
Shreayaa2 <shreyaa2095@gmail.com>
2018-12-03 16:05:28 -0500

    Update Routers/Homeroutes.js

--- Routers/Homeroutes.js
+++ Routers/Homeroutes.js
@@ -10,7 +10,7 @@ var $ = require('jquery');
 var pupil = require('../pupils');
 var tutor = require('../tutors');
 //
-router.post('/', urlencodedParser, j, function(req, res, next){
+router.post('/236', urlencodedParser, j, function(req, res, next){
     tutor.findOne({tcourse: 'COSC236'},'tfname tlname temail', function(err, Tutor) {
         if (err) return res.status(500).send(err)
       return res.status(200).send(Tutor);
